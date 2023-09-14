5ef599b2fa09f1c8fce85ef367d7862feff70cda
Titanium2099 <Titanium2099>
2023-09-13 17:19:24 -0700

    search by get param

--- public/index.js
+++ public/index.js
@@ -1,36 +1,11 @@
 "use strict";
 /**
- * @type {HTMLFormElement}
- */
-const form = document.getElementById("uv-form");
-/**
  * @type {HTMLInputElement}
  */
 const address = document.getElementById("uv-address");
-/**
- * @type {HTMLInputElement}
- */
-const searchEngine = document.getElementById("uv-search-engine");
-/**
- * @type {HTMLParagraphElement}
- */
-const error = document.getElementById("uv-error");
-/**
- * @type {HTMLPreElement}
- */
-const errorCode = document.getElementById("uv-error-code");
+//get the GET parameters
+const params = new URLSearchParams(location.search);
+const finalparams = params.url;
 
-form.addEventListener("submit", async (event) => {
-  event.preventDefault();
-
-  try {
-    await registerSW();
-  } catch (err) {
-    error.textContent = "Failed to register service worker.";
-    errorCode.textContent = err.toString();
-    throw err;
-  }
-
-  const url = search(address.value, searchEngine.value);
+  const url = search(address.value, finalparams);
   location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
-});
